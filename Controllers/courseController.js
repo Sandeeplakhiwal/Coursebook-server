93a77b06ad579d0f50288d82f0de31e562eb3026
Sandeep Lakhiwal <sandeeplakhiwal98@gmail.com>
2023-07-22 17:45:08 +0530

    Change - Selected lectures with getAllCourse

--- Controllers/courseController.js
+++ Controllers/courseController.js
@@ -45,6 +45,16 @@ export const createCourse = catchAssyncError(async (req, res, next) => {
 export const getAllCourses = catchAssyncError(async (req, res, next) => {
   const keyword = req.query.keyword || "";
   const category = req.query.category || "";
+  // const courses = await Course.find({
+  //   title: {
+  //     $regex: keyword,
+  //     $options: "i",
+  //   },
+  //   category: {
+  //     $regex: category,
+  //     $options: "i",
+  //   },
+  // }).select("-lectures");
   const courses = await Course.find({
     title: {
       $regex: keyword,
@@ -54,7 +64,7 @@ export const getAllCourses = catchAssyncError(async (req, res, next) => {
       $regex: category,
       $options: "i",
     },
-  }).select("-lectures");
+  });
   if (!courses) return next(new ErrorHandler("No Courses Yet!"), 404);
   res.status(200).json({
     success: true,

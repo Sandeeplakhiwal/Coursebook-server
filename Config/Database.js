da5fe1a54cbcd38cb0cd2e9092c985d8717c42a6
Sandeep Lakhiwal <sandeeplakhiwal98@gmail.com>
2023-07-23 21:44:01 +0530

    Removed - unnecessary comments

--- Config/Database.js
+++ Config/Database.js
@@ -1,10 +1,3 @@
-// import mongoose from "mongoose";
-
-// export const connectDB = async () => {
-//   const { connection } = await mongoose.connect(process.env.MONGO_URI);
-//   console.log(`MongoDB connected with ${connection.host}`);
-// };
-
 import mongoose from "mongoose";
 
 export const connectDB = async () => {

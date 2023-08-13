02a397458b6b795bd247c34bb374436da4c483fe
Sandeep Lakhiwal <sandeeplakhiwal98@gmail.com>
2023-08-13 13:13:14 +0530

    one change

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 app.use(
   Cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:4173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
